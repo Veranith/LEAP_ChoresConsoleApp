70ed7abf4b08a3e1d14febb6bc838a520969a58d
Ryan Robinson <274507+Veranith@users.noreply.github.com>
2021-10-18 21:27:41 -0600

    Adds inital chore class

---
+++ Chores Library/Chores.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChoresLibrary
+{
+    public class Chores
+    {
+        private SQLConnection choreDb { get; }
+
+        public Chores()
+        {
+            choreDb = new SQLConnection();
+        }
+
+        public void run()
+        {
+            Console.WriteLine(choreDb.connectionString);
+            choreDb.createDatabase("ChoresDB");
+            choreDb.createTable("Chores");
+            addExampleChores();
+        }
+
+        private void addChore(string Name, string Assignment)
+        {
+            choreDb.insertTable(choreDb.tableName, Name, Assignment);
+        }
+
+        private void addExampleChores()
+        {
+            addChore("Wash dishes", "Ryan");
+            addChore("Vacuum", "Ryan");
+            addChore("Dust", null);
+        }
+
+        private void updateChore()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void deleteChore()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void getChores()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
